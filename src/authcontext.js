c08f92bc3dd5e6f06a24ecc474a6990becc75189
gp-1108 <pietrogirotto00@gmail.com>
2022-12-20 20:42:35 +0100

    fixed non-matching object definition

--- src/authcontext.js
+++ src/authcontext.js
@@ -3,8 +3,9 @@ import {onAuthStateChanged} from 'firebase/auth';
 import {auth} from './firebase.js';
 
 export const UserContext = createContext({
-  userPresent: false,
+  userLoaded: false,
   user: null,
+  listener: null,
 });
 
 // eslint-disable-next-line react/prop-types
