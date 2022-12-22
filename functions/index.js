086ccb04a19d8ff8a7e4cd410c05b6153d5790c0
gp-1108 <pietrogirotto00@gmail.com>
2022-12-22 13:32:07 +0100

    added correct return and both db initis

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,29 @@ const {functions, db} = require('./firebase.js');
 // const {collection, addDoc} = require('firebase-admin/firestore');
 
 exports.consoleLog = functions.auth.user().onCreate((user) => {
-  const uid = user.uid;
-  const collectionRef = db.collection('timetables');
-  collectionRef.add({
-    user: uid,
-    table: ['X', 'X', 'X', 'X', 'X', 'X', 'X'],
-  }).then(
-      (docRef) => functions.logger.info('Document written with ID: ', docRef.id)
-  ).catch((err) => functions.logger.error('Error adding document: ', err));
-  return;
+  async function initUser(uid) {
+    const timetableRef = db.collection('timetables');
+    try {
+      await timetableRef.add({
+        user: uid,
+        table: ['X', 'X', 'X', 'X', 'X', 'X', 'X'],
+      });
+      functions.logger.info('User added to database', uid);
+    } catch (error) {
+      functions.logger.error('Error adding user to database', error);
+    }
+    
+    const credentialRef = db.collection('credentials');
+    try {
+      await credentialRef.add({
+        user: uid,
+        credentials: ['', ''],
+      });
+      functions.logger.info('Credential added to database', uid);
+    } catch (error) {
+      functions.logger.error('Credential adding user to database', error);
+    }
+  }
+
+  return initUser(user.uid);
 });
